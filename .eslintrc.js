6eaa6054fcc77cee679a9981bfa66a959095007e
thinh.doduy.tpv <thinh.doduy.tpv@one-line.com>
2024-06-25 11:10:50 +0700

    chore: update eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,13 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
+  ignorePatterns: ['**/*.js'],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
